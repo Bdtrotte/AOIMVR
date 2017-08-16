245f4042f1786dc44886f64dc745a68b0066b5ff
Benjamin Trotter <bdtrotte@ucsc.edu>
2017-08-15 17:58:13 -0700

    Rope wraps around building

--- Assets/justascript.cs
+++ Assets/justascript.cs
@@ -8,19 +8,24 @@ public class justascript : MonoBehaviour
 	public Transform t;
 	public Transform player;
 	public Transform feet;
-	public Transform hookPoint;
 	public Transform head;
+	public Transform hipPoint;
 	public LineRenderer line;
 	public ParticleSystem ps;
-	public bool isRight;
 
 	SteamVR_Controller.Device d;
 
 	bool hasFired = false;
 	float ropeLength = 0;
 
+	List<Transform> linePoints;
+	Vector3 prePos;
+
 	void Start ()
 	{
+		linePoints = new List<Transform> ();
+		prePos = hipPoint.position;
+
 		d = SteamVR_Controller.Input ((int)GetComponent<SteamVR_TrackedObject> ().index);
 		line.enabled = false;
 	}
@@ -38,24 +43,23 @@ public class justascript : MonoBehaviour
 		}
 
 		if (hasFired) {
-			line.SetPosition (0, HipPoint());
-			line.SetPosition (1, hookPoint.position);
+			updateLine ();
+			drawLine ();
 
 			if (d.GetPressDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad)) {
 				hasFired = false;
 				line.enabled = false;
 			} else {
 				float f = d.GetAxis (Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger).x * 4000;
-				r.AddForce ((hookPoint.position - feet.position).normalized * f);
-				Rigidbody rig = hookPoint.parent.gameObject.GetComponent<Rigidbody> ();
+				r.AddForce ((linePoints[linePoints.Count-1].position - feet.position).normalized * f);
+				Rigidbody rig = linePoints[linePoints.Count-1].parent.gameObject.GetComponent<Rigidbody> ();
 				if (rig != null)
-					rig.AddForce ((HipPoint() - hookPoint.position).normalized * f);
+					rig.AddForce ((hipPoint.position - linePoints[linePoints.Count-1].position).normalized * f);
 
-				Vector3 hipPoint = HipPoint ();
-				float distance = Vector3.Distance (hipPoint, hookPoint.position);
+				float distance = TotalDistance ();
 
 				if (distance > ropeLength) {
-					Vector3 hookToHipDir = (hipPoint - hookPoint.position).normalized;
+					Vector3 hookToHipDir = (hipPoint.position - linePoints[linePoints.Count-1].position).normalized;
 
 					if (rig == null) {
 						Vector3 newVelocity = r.velocity - Vector3.Dot (r.velocity, hookToHipDir) * hookToHipDir - hookToHipDir * (distance - ropeLength);
@@ -70,11 +74,11 @@ public class justascript : MonoBehaviour
 
 						rig.velocity = newOtherVelocity;
 
-						Vector3 crossProduct = Vector3.Cross (hookPoint.position - rig.transform.position, hookToHipDir * (distance - ropeLength));
+						Vector3 crossProduct = Vector3.Cross (linePoints[linePoints.Count-1].position - rig.transform.position, hookToHipDir * (distance - ropeLength));
 						rig.angularVelocity = crossProduct*50;
 
-						Vector3 desiredPos = -hookToHipDir * ropeLength + hipPoint;
-						Vector3 hookToOther = rig.transform.position - hookPoint.position;
+						Vector3 desiredPos = -hookToHipDir * ropeLength + hipPoint.position;
+						Vector3 hookToOther = rig.transform.position - linePoints[linePoints.Count-1].position;
 
 						rig.transform.position = desiredPos + hookToOther;
 					}
@@ -86,42 +90,80 @@ public class justascript : MonoBehaviour
 			if (d.GetPressDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger)) {
 				RaycastHit hit;
 				if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity, ~(1 << 8))) {
-					if (Physics.Raycast (HipPoint(), hit.point - HipPoint(), out hit, Mathf.Infinity, ~(1 << 8))) {
-						hookPoint.position = hit.point;
+					if (Physics.Raycast (hipPoint.position, hit.point - hipPoint.position, out hit, Mathf.Infinity, ~(1 << 8))) {
+						Transform hookPoint = new GameObject ("HookPoint").transform;
+						hookPoint.position = hit.point + (hipPoint.position - hit.point).normalized*0.1f;
 						hookPoint.SetParent (hit.collider.transform);
 
-						ropeLength = Vector3.Distance (HipPoint (), hookPoint.position);
+						ropeLength = Vector3.Distance (hipPoint.position, hookPoint.position);
 
 						line.enabled = true;
-						line.SetPosition (0, HipPoint());
-						line.SetPosition (1, hookPoint.position);
+						clearLine ();
+						linePoints.Add (hookPoint);
+						drawLine ();
 
 						hasFired = true;
 					}
 				}
 			}
 		}
+
+		prePos = hipPoint.position;
 	}
 
-	Vector3 HipPoint()
+	void drawLine()
 	{
-		if (isRight)
-			return RightHipPoint ();
-		else
-			return LeftHipPoint ();
+		line.positionCount = linePoints.Count + 1;
+
+		int i = 0;
+		foreach(Transform t in linePoints)
+			line.SetPosition (i++, t.position);
+
+		line.SetPosition (linePoints.Count, hipPoint.position);
 	}
 
-	Vector3 RightHipPoint()
+	void clearLine()
 	{
-		return feet.position
-		+ new Vector3 (0, 1, 0)
-		- new Vector3 (-head.forward.z, 0, head.forward.x).normalized * 0.5f;
+		foreach (Transform t in linePoints)
+			Destroy (t.gameObject);
+
+		linePoints.Clear ();
 	}
 
-	Vector3 LeftHipPoint()
+	void updateLine()
 	{
-		return feet.position
-		+ new Vector3 (0, 1, 0)
-		+ new Vector3 (-head.forward.z, 0, head.forward.x).normalized * 0.5f;
+		if (linePoints.Count > 1) {
+			Vector3 aroundCornerPoint = linePoints[linePoints.Count - 2].position;
+			if (!Physics.Raycast(hipPoint.position, aroundCornerPoint - hipPoint.position, Vector3.Distance(aroundCornerPoint, hipPoint.position), ~(1<<8))) {
+				Destroy(linePoints[linePoints.Count - 1].gameObject);
+				linePoints.RemoveAt(linePoints.Count - 1);
+				return;
+			}
+		}
+
+		if (linePoints [0].GetComponentInParent<Rigidbody> () == null) {
+			Vector3 preHookPoint = linePoints [linePoints.Count - 1].position;
+			RaycastHit hit;
+			if (Physics.Raycast (hipPoint.position, preHookPoint - hipPoint.position, out hit, Vector3.Distance (hipPoint.position, preHookPoint), ~(1 << 8))) {
+				Vector3 newHookPos = hit.point + prePos - hipPoint.position;
+				Transform newHookPoint = new GameObject ("hook point").transform;
+				newHookPoint.position = newHookPos;
+				newHookPoint.SetParent (hit.collider.transform);
+				linePoints.Add (newHookPoint);
+			}
+		}
+	}
+
+	float TotalDistance()
+	{
+		float d = 0;
+		Vector3 prePos = linePoints [0].position;
+
+		for (int i = 1; i < linePoints.Count; ++i) {
+			d += Vector3.Distance (prePos, linePoints [i].position);
+			prePos = linePoints [i].position;
+		}
+
+		return d + Vector3.Distance (prePos, hipPoint.position);
 	}
 }
--- Assets/playBoy.cs
+++ Assets/playBoy.cs
@@ -6,6 +6,12 @@ public class playBoy : MonoBehaviour {
 	public Transform head;
 	public Transform feet;
 
+	public Transform LeftHipPoint;
+	public Transform RightHipPoint;
+
+	public float hipHeight;
+	public float hipDistance;
+
 	void Update()
 	{
 		CapsuleCollider c = GetComponent<CapsuleCollider> ();
@@ -22,5 +28,16 @@ public class playBoy : MonoBehaviour {
 			feet.position = head.position - new Vector3 (0, relativePoint.y, 0);
 			c.center = new Vector3 (relativePoint.x, c.center.y, relativePoint.z);
 		}
+
+		updateHips();
+	}
+
+	void updateHips()
+	{
+		Vector3 right = new Vector3 (head.right.x, 0, head.right.z);
+		right.Normalize ();
+
+		LeftHipPoint.position = -right * hipDistance + feet.position + Vector3.up*hipHeight;
+		RightHipPoint.position = right * hipDistance + feet.position + Vector3.up*hipHeight;
 	}
 }
